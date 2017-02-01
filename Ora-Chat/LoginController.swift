51ae7b8242af39d81f89a0f57669c7cc01bfea52
blsiege <blsiege@gmail.com>
2017-02-01 17:15:29 -0600

    adds login and registration screens and updates splash image

---
+++ Ora-Chat/LoginController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginController.swift
+//  Ora-Chat
+//
+//  Created by Brad Siegel on 2/1/17.
+//  Copyright © 2017 Brad Siegel. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet fileprivate var loginView: LoginView!
+}
---
+++ Ora-Chat/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  Ora-Chat
+//
+//  Created by Brad Siegel on 2/1/17.
+//  Copyright © 2017 Brad Siegel. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    @IBOutlet fileprivate weak var emailTextField: UITextField!
+    
+    @IBOutlet fileprivate weak var passwordTextField: UITextField!
+    
+    @IBAction func login() {
+        
+    }
+    
+    @IBAction func signUp() {
+        
+    }
+    
+}
